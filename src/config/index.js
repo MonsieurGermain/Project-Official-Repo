750492080584e842f78f850dfa61b5ca35040261
Delgee <Stormrage0527@gmail.com>
2022-11-30 03:55:15 +0800

    sync

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,6 @@ dotenv.config();
 export const config = {
   port         : process.env.PORT || 3001,
   serverName   : "escrow",
-  mongoURI     : process.env.MONGO_URI || "mongodb://localhost:27017/escrow",
+  mongoURI     : process.env.MONGO_URI || "mongodb://mongo:27017/escrow",
   sessionSecret: process.env.SESSION_SECRET || "secret",
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,28 @@
-export {};
+import gm from "gm";
+
+const resize = (image, width, height) => {
+  return new Promise((resolve, reject) => {
+    gm(image)
+      .resize(width, height)
+      .toBuffer((err, buffer) => {
+        if (err) reject(err);
+        resolve(buffer);
+      });
+  });
+};
+
+const noProfile = (image) => {
+  return new Promise((resolve, reject) => {
+    gm(image)
+      .noProfile()
+      .toBuffer((err, buffer) => {
+        if (err) reject(err);
+        resolve(buffer);
+      });
+  });
+};
+
+export {
+  resize,
+  noProfile
+};
