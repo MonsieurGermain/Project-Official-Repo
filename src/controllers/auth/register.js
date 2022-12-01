a4555985576008c56d93b95ed611db3a078bf4a2
webdev0505 <webdev0505@gmail.com>
2022-12-01 18:47:15 -0500

    register and login template finished

---
+++ src/controllers/auth/register.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import { copyFile } from "fs";
+
+import { UserModel } from "../../models";
+
+import { generateRandomName } from "../../middlewares/filesUploads";
+
+function createProfilePicture(name) {
+  const randomImgName = generateRandomName(name, 17);
+  const imgPath = `/userImg/${randomImgName}`;
+
+  copyFile(
+    './public/default/default-profile-pic.png',
+    `./uploads${imgPath}`,
+    (err) => {
+      if (err) throw err;
+    },
+  );
+  return imgPath;
+}
+
+const registerController = {
+  get: (req, res) => {
+    res.render("pages/authPages/register");
+  },
+  post: async (req, res) => {
+    try {
+        const { username, password } = req.body;
+    
+        if (await UserModel.findOne({ username })) throw new Error('This Username is Already Taken');
+    
+        const user = new UserModel({
+          username,
+          password: bcrypt.hashSync(password, 12),
+          img_path: createProfilePicture('default-profile-pic.png'),
+        });
+    
+        await user.save();
+    
+        req.flash('success', 'Account Successfully Created');
+        res.redirect('/login');
+      } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+      }
+  }
+};
+
+export { registerController };
